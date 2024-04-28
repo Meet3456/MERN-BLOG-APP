44a692076e7299a0f8ed3bb7d5e840d36a259ad9
Meet3456 <meetvasa09@gmail.com>
2024-04-28 17:21:35 +0530

    mongodb connected

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -59,6 +59,7 @@ export default function Header() {
         </span>
         Blog
       </Link>
+
       <form onSubmit={handleSubmit}>
         <TextInput
           type='text'
@@ -69,9 +70,11 @@ export default function Header() {
           onChange={(e) => setSearchTerm(e.target.value)}
         />
       </form>
+
       <Button className='w-12 h-10 lg:hidden' color='gray' pill>
         <AiOutlineSearch />
       </Button>
+
       <div className='flex gap-2 md:order-2'>
         <Button
           className='w-12 h-10 hidden sm:inline'

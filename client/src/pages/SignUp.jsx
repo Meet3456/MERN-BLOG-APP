79dca3b2a63205929e3c621248cf33c1c4372fcc
Meet3456 <meetvasa09@gmail.com>
2024-04-28 19:09:06 +0530

    complete signup functionality

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,50 +1,67 @@
-import { Alert, Button, Label, Spinner, TextInput } from 'flowbite-react';
-import { useState } from 'react';
-import { Link, useNavigate } from 'react-router-dom';
-import OAuth from '../components/OAuth';
+import { Alert, Button, Label, Spinner, TextInput } from "flowbite-react";
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import OAuth from "../components/OAuth";
 
 export default function SignUp() {
+  // state to track form data
   const [formData, setFormData] = useState({});
+
+  // state to track error message
   const [errorMessage, setErrorMessage] = useState(null);
+
+  // state to track loading state
   const [loading, setLoading] = useState(false);
+
+  // hook to navigate to another page  
   const navigate = useNavigate();
+
+  // handle form input change(changes in the input field while signing up)
   const handleChange = (e) => {
+    // 
     setFormData({ ...formData, [e.target.id]: e.target.value.trim() });
   };
+
+  // handle form submission , send the form data to the server and sign up the user and navigate to the sign in page
   const handleSubmit = async (e) => {
     e.preventDefault();
     if (!formData.username || !formData.email || !formData.password) {
-      return setErrorMessage('Please fill out all fields.');
+      return setErrorMessage("Please fill out all fields.");
     }
     try {
       setLoading(true);
       setErrorMessage(null);
-      const res = await fetch('/api/auth/signup', {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
+
+      // send the form data to the signup endpoint:
+      const res = await fetch("/api/auth/signup", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
         body: JSON.stringify(formData),
       });
+      
+      // res object contains the response from the server, it contains the status of the response, the response headers, the response body, etc.
       const data = await res.json();
       if (data.success === false) {
         return setErrorMessage(data.message);
       }
       setLoading(false);
-      if(res.ok) {
-        navigate('/sign-in');
+      if (res.ok) {
+        navigate("/sign-in");
       }
     } catch (error) {
       setErrorMessage(error.message);
       setLoading(false);
     }
   };
+
   return (
     <div className='min-h-screen mt-20'>
       <div className='flex p-3 max-w-3xl mx-auto flex-col md:flex-row md:items-center gap-5'>
         {/* left */}
         <div className='flex-1'>
           <Link to='/' className='font-bold dark:text-white text-4xl'>
-            <span className='px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white'>
-              Sahand's
+            <span className='px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500  rounded-lg text-white'>
+              Meet's
             </span>
             Blog
           </Link>
@@ -95,7 +112,7 @@ export default function SignUp() {
                   <span className='pl-3'>Loading...</span>
                 </>
               ) : (
-                'Sign Up'
+                "Sign Up"
               )}
             </Button>
             <OAuth />
